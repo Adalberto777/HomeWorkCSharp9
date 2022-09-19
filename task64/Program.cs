22c5c61e206de12b5d753c05c0f1fab025616d9e
Adalberto777 <albert.tliashev@gmail.com>
2022-09-19 21:52:04 +0500

    task64

---
+++ task64/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N
+Console.WriteLine("Введите ганицы итервала");
+int m = int.Parse(Console.ReadLine()!);
+int n = int.Parse(Console.ReadLine()!);
+
+string PrintNumbers(int m, int n)
+{
+if (m == n) return m.ToString();
+return (m + " " + PrintNumbers(m + 1, n));
+}
+
+Console.Write(PrintNumbers(m, n));
